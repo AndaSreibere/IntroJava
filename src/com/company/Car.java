0dbb15da6929aa1b193dada329835525a4fd4e0f
Anda Sreibere <anda.serzante@gmail.com>
2022-04-10 19:32:55 +0200

    JavaClassSgt

---
+++ src/com/company/Car.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Car {
+    //define the attributes of the object
+    private String color = "BLUE";
+    private String model = "w1";
+    private int numOfTires = 4;
+    private boolean engineOn = false;
+
+    //define their behaviours
+    //start engine
+    public void startEngine(boolean key){
+        if (key && !engineOn) {
+            System.out.println("Engine started");
+            engineOn = true;
+        }else{
+            System.out.println("Key not in, cannot start");
+        }
+    }
+
+    public void stopEngine(){
+        if (engineOn){
+            System.out.println("Engine turned off");
+            engineOn = false;
+        } else {
+            System.out.println("Engine already off");
+        }
+    }
+
+}
---
+++ src/com/company/OOPIntro.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class OOPIntro {
+    // A class is a blueprint for describing an object
+    //Car -> attributes: color, model, numOfTires, numOfSeats. Behaviour: startEngine, stopEngine, turnRight, turnLeft.
+
+    //fields  -> used to define attributes in a Class
+}
