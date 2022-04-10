442566b715af200b8f860d8e5bd4c28e657d2773
Anda Sreibere <anda.serzante@gmail.com>
2022-04-10 19:39:00 +0200

    JavaClassSgt

--- src/com/company/OOPIntro.java
+++ src/com/company/OOPIntro.java
@@ -5,4 +5,9 @@ public class OOPIntro {
     //Car -> attributes: color, model, numOfTires, numOfSeats. Behaviour: startEngine, stopEngine, turnRight, turnLeft.
 
     //fields  -> used to define attributes in a Class
+    public static void main(String[] args) {
+        Car benz = new Car(); //crated new instance of existing class
+       benz.startEngine(false);
+        benz.stopEngine();
+    }
 }
